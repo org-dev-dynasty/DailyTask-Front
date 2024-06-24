3f96a422c72eeff1358c3eca7c33c82efb4fcddb
IsaiasCBLuz <isaiascbluz@gmail.com>
2024-06-24 15:22:25 -0300

    feat: Add Feather icons to home screen

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -3,12 +3,17 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 import React from "react";
 import {Container, Line, MiddleContainer, TextMiddle, Title} from "@/app/(tabs)/home/styles";
 
+// Ícones
+import { Feather } from "@expo/vector-icons";
+
 export default function Home() {
     return(
         <>
             <Background>
                 <Container>
-                    <Title >Segure para gravar</Title>
+                    <Title>Segure para gravar</Title>
+                    {/* <Feather name="unlock" size={ 72 } color="white" style={{position: "absolute", top: "15%"}}/> */}
+                    {/* <Feather name="trash" size={ 72 } color="white" style={{position: "absolute", top: "15%", start: 0}}/> */}
 
                     <MaterialCommunityIcons
                         name="microphone-outline"
