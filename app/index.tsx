05447601b5e90c27e4e4373d557457508b4b6dba
IsaiasCBLuz <isaiascbluz@gmail.com>
2024-06-24 15:21:08 -0300

    Addition of "pre navigation" just for development

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,13 @@
+import { Link } from "expo-router";
+import { View } from "react-native";
+
 export default function Index() {
-    return null;
+    return (
+        <>
+            <View style={{flex: 1, width: '100%', height: '100%', justifyContent: "center", padding:16, gap:16}}>
+                <Link href="/home" style={{textAlign: "center", fontSize: 32, borderColor: "black", borderWidth: 2, borderRadius: 15, padding:2}}>Ir para as telas principais</Link>
+                <Link href="/login" style={{textAlign: "center", fontSize: 32, borderColor: "black", borderWidth: 2, borderRadius: 15, padding:2}}>Ir para o Login</Link>
+            </View>
+        </>
+    );
 }
