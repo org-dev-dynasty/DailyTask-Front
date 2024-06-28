d28d8ddaa833bd5140c09caa599e72b2fad54f34
jotaapee <galhardojoaopedro@gmail.com>
2024-06-28 20:41:30 -0300

    feat: Add Task component for rendering tasks in a list
    This commit adds the Task component, which is responsible for rendering individual tasks in a list. It includes the ability to display the task title, hour, description, and color. The component uses styled components for styling and integrates with the Phosphor Icons library for displaying expand/collapse icons.

---
+++ components/tasks/index.tsx
@@ -0,0 +1,60 @@
+import { Animated, TouchableOpacity, View } from "react-native";
+import { TaskDescription, TaskDescriptionText, TaskHeader, TaskText, TaskContainer, ColorSection, MainSection } from "./styles";
+import { CaretDown, CaretUp } from "@phosphor-icons/react";
+import { useState } from "react";
+
+export type TaskProps = {
+    title: string;
+    hour?: string;
+    description?: string;
+    color?: string;
+    secondaryColor?: string;
+}
+
+export default function Task({ hour, title, description, color, secondaryColor }: TaskProps) {
+    const [expanded, setExpanded] = useState(false);
+    const [height, setHeight] = useState(new Animated.Value(50));
+
+    const toggleExpandCard = () => {
+        if (expanded) {
+            Animated.timing(height, {
+                toValue: 50,
+                duration: 300,
+                useNativeDriver: false,
+            }).start();
+        } else {
+            Animated.timing(height, {
+                toValue: 120, 
+                duration: 300,
+                useNativeDriver: false,
+            }).start();
+        }
+        setExpanded(!expanded);
+    };
+
+    return (
+        <Animated.View style={{ height, marginBottom: 20 }}>
+            <TaskContainer>
+                <ColorSection style={{ backgroundColor: secondaryColor }} />    
+                <MainSection style={{ backgroundColor: color }}>
+                    <TaskHeader>
+                        <TaskText>{title}</TaskText>
+                        <TaskText style={{ marginHorizontal: 10 }}>{hour}</TaskText>
+                        <TouchableOpacity onPress={toggleExpandCard}>
+                            {expanded ? (
+                                <CaretUp size={24} style={{ color: 'white' }} />
+                            ) : (
+                                <CaretDown size={24} style={{ color: 'white' }} />
+                            )}
+                        </TouchableOpacity>
+                    </TaskHeader>
+                    {expanded && (
+                        <TaskDescription>
+                            <TaskDescriptionText>{description}</TaskDescriptionText>
+                        </TaskDescription>
+                    )}
+                </MainSection>
+            </TaskContainer>
+        </Animated.View>
+    );
+}
