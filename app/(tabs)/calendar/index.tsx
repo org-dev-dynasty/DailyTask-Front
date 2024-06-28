caf48862605b7e651bda3ab9a82180a63e8660fb
jotaapee <galhardojoaopedro@gmail.com>
2024-06-28 17:29:14 -0300

    refactor: Update calendar screen styles
    This commit refactors the styles in the `calendar/styles.ts` file. The changes include removing commented out code, updating padding and margin values, and introducing new styled components for the calendar screen. This improves the readability and maintainability of the code.

--- app/(tabs)/calendar/index.tsx
+++ app/(tabs)/calendar/index.tsx
@@ -1,167 +1,78 @@
-import { Background } from "@/components/background";
-import { Container, Section, TaskText } from "./styles";
-import { TaskSection } from "@/components/taskSection";
-import React, { useRef, useCallback } from 'react';
-import { ExpandableCalendar, AgendaList, CalendarProvider, WeekCalendar } from 'react-native-calendars';
-import { Dimensions, FlatList, ScrollView, View } from 'react-native';
+import { Background } from "@/components/background"
+import CalendarTasks from "@/components/calendarTasks";
+import { CaretDown } from "@phosphor-icons/react";
+import { FlatList, TouchableOpacity, View } from "react-native"
 
 
-// const leftArrowIcon = require('../../../assets/appImages/previous.png');
-// const rightArrowIcon = require('../../../assets/appImages/next.png');
-
-const taskSections = [
-  { label: 'Hoje - Manhã', tasks: [{ title: 'Prova de Calculo', color: '#2F9CD8', content: 'Conteúdo da manhã de hoje' }] },
-  { label: 'Tarde', tasks: [{ title: 'Estudar', color: '#70CE99', content: 'Conteúdo da tarde' }] },
-  { label: 'Noite', tasks: [{ title: 'Jantar', color: '#EB5757', content: 'Conteúdo da noite' }] },
-];
-
-type ItemProps = { label: string, tasks: any, index: number };
-
-const Item = ({ label, tasks, index }: ItemProps) => (
-  <Container style={{ flex: 1 }}>
-    <TaskSection key={index} label={label} tasks={tasks} />
-    <Section>
-      <TaskText>Março de 11 a Março de 16</TaskText>
-      <TaskText>Nada ainda</TaskText>
-    </Section>
-  </Container>
-);
-
-export default function Calendario() {
-
-
-  return (
-    <Background style={{ flex: 1  }}>
-      <View style={{ width: '100%', position: 'static' }}>
-        <CalendarProvider date="2024-06-27" style={{ width: '100%' }} >
-          <ExpandableCalendar
-            initialPosition={ExpandableCalendar.positions.OPEN}
-            firstDay={1}
-            markedDates={{
-              '2023-03-10': { selected: true, marked: true },
-              '2023-03-11': { marked: true },
-              '2023-03-12': { marked: true, dotColor: 'red', activeOpacity: 0 },
-            }}
-
-          />
-          {/* <WeekCalendar /> */}
-        </CalendarProvider>
-      </View>
-      {/* <FlatList
-        data={taskSections}
-        renderItem={({item}) => <Item label={item.label} tasks={item.tasks} index={0} />}
-        style={{ width: '100%' }} 
-      /> */}
-      <ScrollView style={{ width: '100%', height: '30%', paddingTop: 10 }}>
-        <Container style={{ flex: 1, alignItems: 'center', position: 'static' }} >
-          {taskSections.map((section: any, index: any) => (
-            <TaskSection key={index} label={section.label} tasks={section.tasks} />
-          ))}
-        <Section>
-            <TaskText>Março de 11 a Março de 16</TaskText>
-            <TaskText>Nada ainda</TaskText>
-          </Section>
-        </Container>
-      </ScrollView>
-    </Background>
-  );
-}
-
-// import React, {useRef, useCallback} from 'react';
-// import {StyleSheet} from 'react-native';
-// import {ExpandableCalendar, AgendaList, CalendarProvider, WeekCalendar} from 'react-native-calendars';
-// import testIDs from '../testIDs';
-// import {agendaItems, getMarkedDates} from '../mocks/agendaItems';
-// import AgendaItem from '../mocks/AgendaItem';
-// import {getTheme, themeColor, lightThemeColor} from '../mocks/theme';
-
-// const leftArrowIcon = require('../img/previous.png');
-// const rightArrowIcon = require('../img/next.png');
-// const ITEMS: any[] = agendaItems;
-
-// interface Props {
-//   weekView?: boolean;
+// type ItemProps = {
+//   id: number;
+//   label: string;
+//   title: string;
+//   hour?: string;
+//   description?: string;
+//   color?: string;
 // }
 
-// const ExpandableCalendarScreen = (props: Props) => {
-//   const {weekView} = props;
-//   const marked = useRef(getMarkedDates());
-//   const theme = useRef(getTheme());
-//   const todayBtnTheme = useRef({
-//     todayButtonTextColor: themeColor
-//   });
+const data = [
+  {
+    id: 1,
+    label: 'Hoje - Manha',
+    title: 'Prova de calculo',
+    hour: '08:00',
+    color: '#2F9CD8',
+    description: 'Tenho uma prova de calculo hoje, preciso estudar bastante para conseguir uma boa nota'
+  },
+  {
+    id: 2,
+    label: 'Tarde',
+    title: 'Reuniao de projeto',
+    hour: '14:00',
+    color: '#70CE99',
+    description: 'Reuniao de projeto com o time de desenvolvimento, vamos discutir sobre o andamento do projeto'
+  },
+  {
+    id: 3,
+    label: 'Noite',
+    title: 'Entrega de projeto',
+    hour: '22:00',
+    color: '#EB5757',
+    description: 'Entrega do projeto de calculo, preciso finalizar os ultimos detalhes para entregar'
+  },
+]
 
-//   // const onDateChanged = useCallback((date, updateSource) => {
-//   //   console.log('ExpandableCalendarScreen onDateChanged: ', date, updateSource);
-//   // }, []);
-
-//   // const onMonthChange = useCallback(({dateString}) => {
-//   //   console.log('ExpandableCalendarScreen onMonthChange: ', dateString);
-//   // }, []);
-
-//   const renderItem = useCallback(({item}: any) => {
-//     return <AgendaItem item={item}/>;
-//   }, []);
-
-//   return (
-//     <CalendarProvider
-//       date={ITEMS[1]?.title}
-//       // onDateChanged={onDateChanged}
-//       // onMonthChange={onMonthChange}
-//       showTodayButton
-//       // disabledOpacity={0.6}
-//       theme={todayBtnTheme.current}
-//       // todayBottomMargin={16}
-//     >
-//       {weekView ? (
-//         <WeekCalendar testID={testIDs.weekCalendar.CONTAINER} firstDay={1} markedDates={marked.current}/>
-//       ) : (
-//         <ExpandableCalendar
-//           testID={testIDs.expandableCalendar.CONTAINER}
-//           // horizontal={false}
-//           // hideArrows
-//           // disablePan
-//           // hideKnob
-//           // initialPosition={ExpandableCalendar.positions.OPEN}
-//           // calendarStyle={styles.calendar}
-//           // headerStyle={styles.header} // for horizontal only
-//           // disableWeekScroll
-//           theme={theme.current}
-//           // disableAllTouchEventsForDisabledDays
-//           firstDay={1}
-//           markedDates={marked.current}
-//           leftArrowImageSource={leftArrowIcon}
-//           rightArrowImageSource={rightArrowIcon}
-//           // animateScroll
-//           // closeOnDayPress={false}
-//         />
-//       )}
-//       <AgendaList
-//         sections={ITEMS}
-//         renderItem={renderItem}
-//         // scrollToNextEvent
-//         sectionStyle={styles.section}
-//         // dayFormat={'yyyy-MM-d'}
-//       />
-//     </CalendarProvider>
-//   );
-// };
-
-// export default ExpandableCalendarScreen;
-
-// const styles = StyleSheet.create({
-//   calendar: {
-//     paddingLeft: 20,
-//     paddingRight: 20
-//   },
-//   header: {
-//     backgroundColor: 'lightgrey'
-//   },
-//   section: {
-//     backgroundColor: lightThemeColor,
-//     color: 'grey',
-//     textTransform: 'capitalize'
-//   }
-// });
+// const Item = ({ label, hour, title, description, color }: ItemProps) => (
+//   <>
+//     <Container>
+//       <LabelView>
+//         <LabelText>{label}</LabelText>
+//         <TouchableOpacity>
+//           <CaretDown size={24} style={{ color: 'white' }} />
+//         </TouchableOpacity>
+//       </LabelView>
+//       <View style={{ backgroundColor: {color} }}>
+//         <TaskHeader>
+//           <TaskText>{title}</TaskText>
+//           <TaskText>{hour}</TaskText>
+//           <TouchableOpacity>
+//             <CaretDown size={24} style={{ color: 'white' }} />
+//           </TouchableOpacity>
+//         </TaskHeader>
+//         <TaskDescription>
+//           <TaskDescriptionText>{description}</TaskDescriptionText>
+//         </TaskDescription>
+//       </View>
+//     </Container>
+//   </>
+// )
 
 
+export default function Calendar() {
+  return (
+    <Background>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => <CalendarTasks id={item.id} title={item.title} hour={item.hour} description={item.description} color={item.color} label={item.label} />}
+      />
+    </Background>
+  )
+}
--- app/(tabs)/calendar/styles.ts
+++ app/(tabs)/calendar/styles.ts
@@ -1,53 +1,38 @@
-// import styled from 'styled-components/native';
-
-// export const Container = styled.View`
-//   flex: 1;
-//   width: 100%;
-//   justify-content: flex-end;
-//   align-items: center;
-//   margin-bottom: 20px;
-// `;
-
-// export const Section = styled.View`
-//   margin-bottom: 20px;
-//   width: 70%;
-// `;
-
-// export const SectionTitle = styled.Text`
-//     color: white;
-//     font-size: 16px;
-//     margin-bottom: 10px;
-//     margin-right: 10px;
-// `;
-
-// export const Task = styled.View`
-//   height: 50px;
-//   border-radius: 10px;
-//   margin-bottom: 10px;
-//   width: 100%; 
-//   align-items: center;
-// `;
-
-// export const TaskText = styled.Text`
-//   color: white;
-//   font-size: 14px;
-//   margin-bottom: 5px;
-//   /* align-self: flex-start; */
-// `;
-
-import styled from 'styled-components/native';
+import styled from "styled-components/native";
 
 export const Container = styled.View`
-  flex: 1;
-  padding: 10px;
+  width: 100%;
+  padding: 15px;
 `;
 
-export const Section = styled.View`
-  margin: 10px 0;
+
+export const LabelView = styled.View`
+  flex-direction: row;
+  gap: 20px;
+  align-items: center;
+  padding: 5px;
+`;
+
+export const LabelText = styled.Text`
+  color: white;
+`
+
+export const TaskHeader = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
 `;
 
 export const TaskText = styled.Text`
   color: white;
-  font-size: 16px;
-  margin-top: 5px;
+  font-size: 14px;
+`;
+
+export const TaskDescription = styled.View`
+  padding: 10px;
+`;
+
+export const TaskDescriptionText = styled.Text`
+  color: white;
 `;
