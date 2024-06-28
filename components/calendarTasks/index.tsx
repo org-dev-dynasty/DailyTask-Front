0942fe7bf2eeddfaca967aa9d2a911f20f495a6c
jotaapee <galhardojoaopedro@gmail.com>
2024-06-28 20:41:07 -0300

    feat: Update CalendarTasks component to use new data structure

--- components/calendarTasks/index.tsx
+++ components/calendarTasks/index.tsx
@@ -1,39 +1,73 @@
-import { CaretDown } from "@phosphor-icons/react";
-import { Container, LabelText, LabelView, TaskDescription, TaskDescriptionText, TaskHeader, TaskText } from "./styles";
-import { FlatList, TouchableOpacity, View } from "react-native"
+import { CaretDown, CaretUp } from "@phosphor-icons/react";
+import { Container, LabelContainer, LabelText, LabelView } from "./styles";
+import { Animated, FlatList, TouchableOpacity, View } from "react-native";
+import { useState } from "react";
+import Task, { TaskProps } from "../tasks";
 
-type ItemProps = {
-    id: number;
+
+export type ItemProps = {
     label: string;
-    title: string;
-    hour?: string;
-    description?: string;
-    color?: string;
-  }
+    tasks: TaskProps[];
+}
 
-export default function CalendarTasks({ label, hour, title, description, color }: ItemProps) {
+export default function CalendarTasks({ label, tasks }: ItemProps) {
+    const [expanded, setExpanded] = useState(false);
+    const [height] = useState(new Animated.Value(70));
+
+    const toggleExpandLabel = () => {
+        if (expanded) {
+            Animated.timing(height, {
+                toValue: 70,
+                duration: 300,
+                useNativeDriver: false,
+            }).start();
+        } else {
+            Animated.timing(height, {
+                toValue: 70,
+                duration: 300,
+                useNativeDriver: false,
+            }).start();
+        }
+        setExpanded(!expanded);
+    };
+
     return (
         <>
             <Container>
-                <LabelView>
-                    <LabelText>{label}</LabelText>
-                        <TouchableOpacity>
-                            <CaretDown size={24} style={{ color: 'white' }} />
+                <LabelContainer style={{ height }}>
+                    <LabelView>
+                        <LabelText>{label}</LabelText>
+                        <TouchableOpacity onPress={toggleExpandLabel}>
+                            {expanded ? (
+                                <CaretUp size={24} style={{ color: 'white' }} />
+                            ) : (
+                                <CaretDown size={24} style={{ color: 'white' }} />
+                            )}
                         </TouchableOpacity>
-                </LabelView>
-                <View style={{ backgroundColor: color  }}>
-                    <TaskHeader>
-                        <TaskText>{title}</TaskText>
-                        <TaskText>{hour}</TaskText>
-                        <TouchableOpacity>
-                            <CaretDown size={24} style={{ color: 'white' }} />
-                        </TouchableOpacity>
-                    </TaskHeader>
-                    <TaskDescription>
-                        <TaskDescriptionText>{description}</TaskDescriptionText>
-                    </TaskDescription>
-                </View>
+                    </LabelView>
+                    <View style={{ width: '100%' }}>
+                        {tasks.length > 0 && (
+                            <Task
+                                title={tasks[0].title}
+                                hour={tasks[0].hour}
+                                description={tasks[0].description}
+                                color={tasks[0].color}
+                                secondaryColor={tasks[0].secondaryColor}
+                            />
+                        )}
+                        {expanded && tasks.slice(1).map((task, index) => (
+                            <Task
+                                key={index}
+                                title={task.title}
+                                hour={task.hour}
+                                description={task.description}
+                                color={task.color}
+                                secondaryColor={task.secondaryColor}
+                            />
+                        ))}
+                    </View>
+                </LabelContainer>
             </Container>
         </>
-    )
+    );
 }
--- components/calendarTasks/styles.ts
+++ components/calendarTasks/styles.ts
@@ -1,38 +1,29 @@
+import { Animated } from "react-native";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
   width: 100%;
   padding: 15px;
+  height: 50%;
 `;
 
 
-export const LabelView = styled.View`
-  flex-direction: row;
+export const LabelContainer = styled(Animated.View)`
+  flex-direction: column;
   gap: 20px;
   align-items: center;
   padding: 5px;
+  margin-bottom: 10px;
+  width: 100%
 `;
+
+export const LabelView = styled.View`
+    flex-direction: row;
+    width: 100%;
+    gap: 10px;
+`
 
 export const LabelText = styled.Text`
   color: white;
+  font-size: 16px;
 `
-
-export const TaskHeader = styled.View`
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px;
-`;
-
-export const TaskText = styled.Text`
-  color: white;
-  font-size: 14px;
-`;
-
-export const TaskDescription = styled.View`
-  padding: 10px;
-`;
-
-export const TaskDescriptionText = styled.Text`
-  color: white;
-`;
