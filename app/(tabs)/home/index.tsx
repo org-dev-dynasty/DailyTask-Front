7ddc5f802fc4700eb126574431b7aaa48171f6d2
IsaiasCBLuz <isaiascbluz@gmail.com>
2024-06-29 06:55:11 -0300

    chore: Update home screen styles and structure

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,49 +1,103 @@
-import {Background} from "@/components/background";
+import { Background } from "@/components/background";
 import React from "react";
-import {Container, Line, MiddleContainer, TextMiddle, Title, TopView, LockIcon} from "@/app/(tabs)/home/styles";
-import { TouchableOpacity, View } from "react-native";
+import { TouchableOpacity, View, Animated, Text } from "react-native";
+import { Container, TopViewText, Title, InitialScreen, Line, MiddleContainer, TextMiddle, TopViewLock, LockIcon, RecordingTime, RecordingTextInput, RecordingTextInputText } from "@/app/(tabs)/home/styles";
+import theme from "@/themes/theme";
 
 // Ícones
-import {Microphone, Keyboard, LockSimpleOpen} from "phosphor-react-native"
-import theme from "@/themes/theme";
+import { Microphone, Keyboard, LockSimpleOpen } from "phosphor-react-native";
 
 export default function Home() {
     const [recording, setRecording] = React.useState(false);
 
-    function startRecording() {
+    const fadeLock = React.useRef(new Animated.Value(0)).current;
+    const fadeTexts = React.useRef(new Animated.Value(1)).current;
 
+    const StartRecordingAnimation = () => {
+        Animated.timing(fadeLock, {
+            toValue: 1,
+            duration: 1000,
+            useNativeDriver: true,
+        }).start();
+        Animated.timing(fadeTexts, {
+            toValue: 0,
+            duration: 2000,
+            useNativeDriver: true,
+        }).start();
+    };
+
+    function startRecording() {
+        StartRecordingAnimation();
+        setTimeout(() => {
+            setRecording(true);
+        }, 2100);
     }
 
-    return(
+    return (
         <>
             <Background>
                 <Container>
-                    <TopView>
-                        <Title>Segure para gravar</Title>
-                        <LockIcon>
-                            <LockSimpleOpen size={ 64 } color="white" />
-                        </LockIcon>
-                    </TopView>
+                    {/* <--Modal--> */}
+                    {/* <--Modal--> */}
+                    {/* <--Initial--> */}
+                    <InitialScreen style={{ display: recording ? "none" : "flex" }}>
+                        <TopViewLock>
+                            <Animated.View style={{ opacity: fadeLock }}>
+                                <LockIcon>
+                                    <LockSimpleOpen size={64} color={theme.COLORS.WHITE} />
+                                </LockIcon>
+                            </Animated.View>
+                        </TopViewLock>
+                        <TopViewText>
+                            <Animated.View style={{ opacity: fadeTexts }}>
+                                <Title>Segure para gravar</Title>
+                            </Animated.View>
+                        </TopViewText>
+                        <InitialScreen>
+                            <TouchableOpacity onPress={() => startRecording()}>
+                                <Microphone size={100} color={theme.COLORS.WHITE} />
+                            </TouchableOpacity>
+                            <Animated.View style={{ opacity: fadeTexts }}>
+                                <MiddleContainer>
+                                    <Line />
+                                    <TextMiddle>ou</TextMiddle>
+                                    <Line />
+                                </MiddleContainer>
+                            </Animated.View>
+                            <Animated.View style={{ opacity: fadeTexts }}>
+                                <TouchableOpacity>
+                                    <Keyboard size={100} color={theme.COLORS.WHITE} />
+                                </TouchableOpacity>
+                            </Animated.View>
+                        </InitialScreen>
+                    </InitialScreen>
+                    {/* <--Initial--> */}
+                    {/* <--Recording--> */}
+                    <InitialScreen style={{ display: recording ? "flex" : "none" }}>
+                        <TopViewLock>
+                            <Animated.View>
+                                <LockIcon>
+                                    <LockSimpleOpen size={64} color={theme.COLORS.WHITE} />
+                                </LockIcon>
+                            </Animated.View>
+                        </TopViewLock>
 
-                    <TouchableOpacity>
-                        <Microphone
-                            size={ 72 }
-                            color="white"
-                        />
-                    </TouchableOpacity>
+                        <InitialScreen>
+                            <TouchableOpacity onPress={() => startRecording()}>
+                                <Microphone size={100} color={theme.COLORS.WHITE} />
+                            </TouchableOpacity>
 
-                    <MiddleContainer>
-                        <Line />
-                        <TextMiddle >ou</TextMiddle>
-                        <Line />
-                    </MiddleContainer>
+                            <MiddleContainer>
+                                <RecordingTime>0:00</RecordingTime>
+                            </MiddleContainer>
 
-                    <TouchableOpacity>
-                        <Keyboard
-                            size={ 72 }
-                            color="white"
-                        />
-                    </TouchableOpacity>
+                            <RecordingTextInput>
+                                <RecordingTextInputText>Lorem</RecordingTextInputText>
+                            </RecordingTextInput>
+
+                        </InitialScreen>
+                    </InitialScreen>
+                    {/* <--Recording--> */}
                 </Container>
             </Background>
         </>
--- app/(tabs)/home/styles.ts
+++ app/(tabs)/home/styles.ts
@@ -7,17 +7,33 @@ export const Container = styled.View`
   margin: 0;
   top: 32px;
   padding: 0;
-  justify-content: center;
+  padding-top: 40px;
   align-items: center;
   height: 100%;
   width: 100%;
 `;
 
-export const MiddleContainer = styled.View`
-    flex-direction: row;
+export const TopViewText = styled.View`
+    padding: 0;
+    top: 20%;
+    position: absolute;
     justify-content: center;
     align-items: center;
-    margin: 20px 0;
+    width: 100%;
+`;
+
+export const Title = styled.Text`
+    color: ${theme.COLORS.WHITE};
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    font-size: ${theme.FONT_SIZE.XL};
+`;
+
+export const InitialScreen = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    z-index: 1;
 `;
 
 export const Line = styled.View`
@@ -26,10 +42,11 @@ export const Line = styled.View`
     background-color: ${theme.COLORS.WHITE};
 `;
 
-export const Title = styled.Text`
-    color: ${theme.COLORS.WHITE};
-    font-family: ${theme.FONT_FAMILY.REGULAR};
-    font-size: ${theme.FONT_SIZE.XL};
+export const MiddleContainer = styled.View`
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    margin: 20px 0;
 `;
 
 export const TextMiddle = styled.Text`
@@ -40,21 +57,47 @@ export const TextMiddle = styled.Text`
     padding-bottom: 2px;
 `;
 
-export const TopView = styled.View`
+export const TopViewLock = styled.View`
     padding: 0;
     top: 0;
     position: absolute;
     justify-content: center;
     align-items: center;
     width: 100%;
-    height: 40%;
 `;
 
 export const LockIcon = styled.View`
-    width: 100;
-    height: 100;
+    width: 100px;
+    height: 100px;
     background-Color: ${theme.COLORS.MAIN};
     border-Radius: 90px;
     justify-Content: center;
     align-Items: center;
+`;
+
+export const RecordingTime = styled.Text`
+    color: ${theme.COLORS.WHITE};
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    font-size: ${theme.FONT_SIZE.XL};
+`;
+
+export const RecordingTextInput = styled.View`
+    width: 80%;
+    background-color: ${theme.COLORS.WHITE};
+    border-radius: 15px;
+    padding: 10px;
+    margin: 20px 0;
+`;
+
+export const RecordingTextInputText = styled.TextInput.attrs({
+    multiline: true,
+    maxLength: 1200,
+    numberOfLines: 1,
+    scrollEnabled: true,
+})`
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    font-size: ${theme.FONT_SIZE.MD};
+    color: ${theme.COLORS.BLACK};
+    height: auto;
+    max-height: 100px;
 `;
