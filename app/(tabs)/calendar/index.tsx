58650cfffb7d23c8f146be2c0dd1030a3f401f8c
IsaiasCBLuz <isaiascbluz@gmail.com>
2024-07-11 20:24:12 -0300

    style: Update calendar styles

--- app/(tabs)/calendar/index.tsx
+++ app/(tabs)/calendar/index.tsx
@@ -1,48 +1,139 @@
+import React from 'react';
+import { Background } from "@/components/background";
+import { Calendar } from 'react-native-calendars';
+import { Dimensions } from 'react-native';
+import { Bar, CalendarComponent, CalendarContainer, TaskLabel, TasksContainer, TextLabel } from './styles';
+import { View, Text } from 'react-native';
+import { LocaleConfig } from 'react-native-calendars';
+import theme from '@/themes/theme';
+import { CaretDown, CaretUp } from "phosphor-react-native"
+import { TaskCard }  from '@/components/taskCard';
 
-import { Background } from "@/components/background"
-import { ItemProps } from "@/components/calendarTasks";
+// Configurando a localização para português
+LocaleConfig.locales['pt-br'] = {
+  monthNames: [
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro'
+  ],
+  monthNamesShort: [
+    'Jan.',
+    'Fev.',
+    'Mar.',
+    'Abr.',
+    'Mai.',
+    'Jun.',
+    'Jul.',
+    'Ago.',
+    'Set.',
+    'Out.',
+    'Nov.',
+    'Dez.'
+  ],
+  dayNames: [
+    'Domingo',
+    'Segunda-feira',
+    'Terça-feira',
+    'Quarta-feira',
+    'Quinta-feira',
+    'Sexta-feira',
+    'Sábado'
+  ],
+  dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+  today: 'Hoje'
+};
 
-import { useState } from "react";
-import { FlatList } from "react-native"
-import { View, Text, StyleSheet } from 'react-native';
+LocaleConfig.defaultLocale = 'pt-br';
 
+export default function CalendarTasks() {
+  const { width, height } = Dimensions.get('window');
 
-const data: Array<ItemProps> = [
-  {
-    date: '20224-07-02',
-    tasks: [
-      {
-        title: 'Reuniao de projeto',
-        hour: '14:00',
-        color: '#70CE99',
-        secondaryColor: '#4A9C5A',
-        description: 'Reuniao de projeto com o time de desenvolvimento, vamos discutir sobre o andamento do projeto'
-      },
-      {
-        title: 'Prova de calculo',
-        hour: '08:00',
-        color: '#2F9CD8',
-        secondaryColor: '#1A6A9C',
-        description: 'Tenho uma prova de calculo hoje, preciso estudar bastante para conseguir uma boa nota'
-      }
-    ]
-  },
-]
+  // Confg of the dots 
+  const vacation = {key: 'vacation', color: 'red', selectedDotColor: 'blue'};
+  const massage = {key: 'massage', color: 'blue', selectedDotColor: 'blue'};
+  const workout = {key: 'workout', color: 'green'};
 
-
-export default function Calendar() {
-  const [open, setOpen] = useState(false) 
+  // tasks
 
   return (
     <Background>
-      <Calendar
-      
-      />
-        {/* <FlatList
-        data={data}
-        renderItem={({ item }) => <CalendarTasks label={item.label} tasks={item.tasks} />}
-        style={{ width: '90%' }}
-      /> */}
+      <CalendarContainer>
+        <CalendarComponent
+          style={{ width: width/100*90 }}
+          theme={{
+            calendarBackground: '#310842',
+            textSectionTitleColor: theme.COLORS.WHITE,
+            selectedDayBackgroundColor: theme.COLORS.MAIN,
+            selectedDayTextColor: theme.COLORS.WHITE,
+            todayTextColor: theme.COLORS.MAIN,
+            dayTextColor: theme.COLORS.WHITE,
+            textDisabledColor: '#6e6c7e',
+            monthTextColor: theme.COLORS.WHITE,
+            indicatorColor: theme.COLORS.WHITE,
+            textDayFontFamily: theme.FONT_FAMILY.MEDIUM,
+            textMonthFontFamily: theme.FONT_FAMILY.MEDIUM,
+            textDayHeaderFontFamily: theme.FONT_FAMILY.MEDIUM,
+            textDayFontWeight: theme.FONT_SIZE.SM,
+            textMonthFontWeight: theme.FONT_SIZE.MD,
+            textDayHeaderFontWeight: theme.FONT_SIZE.SM,
+            textDayFontSize: 16,
+            textMonthFontSize: 20,
+            textDayHeaderFontSize: 14
+          }}
+          // Handler which gets executed on day press. Default = undefined
+          onDayPress={(day) => {
+            console.log('selected day', day);
+          }}
+          // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+          monthFormat={'MMMM yyyy'}
+          // Handler which gets executed when visible month changes in calendar. Default = undefined
+          onMonthChange={(month) => {
+            console.log('month changed', month);
+          }}
+          // Hide month navigation arrows. Default = false
+          hideArrows={false}
+          // Replace default arrows with custom ones (direction can be 'left' or 'right')
+          renderArrow={(direction) => (
+            <Text style={{ color: '#ffffff', fontSize: 24 }}>{direction === 'left' ? '<' : '>'}</Text>
+          )}
+          // Do not show days of other months in month page. Default = false
+          hideExtraDays={true}
+          // If hideArrows=false and hideExtraDays=false do not switch month when tapping on greyed out
+          // day from another month that is visible in calendar page. Default = false
+          disableMonthChange={true}
+          // Hide day names. Default = false
+          hideDayNames={false}
+          // Show week numbers to the left. Default = false
+          showWeekNumbers={false}
+          // Collection of dates that have to be marked. Default = {}
+          markingType={'multi-dot'}
+          markedDates={{
+            '2024-07-16': {dots: [vacation, massage, workout], selected: true, selectedColor: theme.COLORS.MAIN},
+            '2024-07-17': {marked: true},
+            '2024-07-18': {marked: true, dotColor: 'red', activeOpacity: 0},
+            '2024-07-19': {disabled: true, disableTouchEvent: true}
+          }}
+        />
+        <Bar/>
+      </CalendarContainer>
+      <TasksContainer>
+        <View>
+          <TaskLabel>
+            <TextLabel>Hoje - Manhã</TextLabel>
+            <CaretDown size={24} color={theme.COLORS.WHITE} weight="bold"/>
+          </TaskLabel>
+          <TaskCard title='Médico' description='Ir ao consultório x as 17:20, pq se não a ernia vai ficar pior, e eu vou morrer, e etc, socorrooooo' date='10/11/2024' time='17:20' status='open' color='#2F9CD8' color2='#9CD4F4' ></TaskCard>
+        </View>
+      </TasksContainer>
     </Background>
-  )
+  );
 }
---
+++ app/(tabs)/calendar/styles.ts
@@ -0,0 +1,52 @@
+import styled from "styled-components/native";
+import theme from "@/themes/theme";
+import { Calendar } from 'react-native-calendars';
+
+export const CalendarContainer = styled.View`
+  position: absolute;
+  top: 0;
+  width: 100%;
+  background-color: #310842;
+  border-radius: 20px;
+  padding: 20px;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const CalendarComponent = styled(Calendar)`
+  background-color: transparent;
+`;
+
+export const Bar = styled.View`
+    width: 45%;
+    height: 10px;
+    background-color: ${theme.COLORS.WHITE};
+    margin-top: 10px;
+    border-radius: 360px;
+`;
+
+export const TasksContainer = styled.View`
+    position: absolute;
+    top: 50%;
+    width: 90%;
+    padding: 20px;
+    flex-direction: column;
+    margin-top: 20px;
+
+    background-color: red;
+`;
+
+export const TaskLabel = styled.View`
+    width: 100%;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+    margin-bottom: 10px;
+`;
+
+export const TextLabel = styled.Text`
+    color: ${theme.COLORS.WHITE};
+    font-size: ${theme.FONT_SIZE.LG};
+    font-weight: ${theme.FONT_FAMILY.MEDIUM};
+    margin-right: 10px;
+`;
