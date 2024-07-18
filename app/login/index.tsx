47e07e1a42c5a0718116acd8d737d5967f089660
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-07-18 11:18:29 -0300

    added google auth init with response correct

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -2,21 +2,45 @@ import { CenteredView, ForgotPasswordText, InputView, LoginButton, LoginButtonTe
 import { Background } from '@/components/background';
 // import Icon from 'react-native-vector-icons/FontAwesome';
 import { Link } from 'expo-router';
-import { Image, Text, View } from 'react-native';
+import { Alert, Image, Text, TouchableOpacity, View } from 'react-native';
 import { useEffect, useState } from 'react';
 import { TextInput } from 'react-native-paper';
 import { Input } from '@/components/input/input';
+
+// import * as WebBrowser from 'expo-web-browser'
+// import * as Google from 'expo-auth-session/providers/google'
+// import * as AuthSession from 'expo-auth-session'
+
+import { GoogleSignin } from "@react-native-google-signin/google-signin"
 
 const Logo = require('../../assets/appImages/logo-daily-branca.png');
 const LogoDevDynasty = require('../../assets/appImages/logo-dev-dynasty.png');
 const LogoGoogle = require('../../assets/appImages/logo-google.png');
 const LogoGitHub = require('../../assets/appImages/logo-gitHub.png');
 
+GoogleSignin.configure({
+  scopes: ['email', 'profile'],
+  webClientId: '477785692735-olm3sejb8innr21hts1affdfrso98m8o.apps.googleusercontent.com',
+  iosClientId: '477785692735-m38lmcvn3lp2u4q2r0m7hi4nquim8gjq.apps.googleusercontent.com',
+})
+
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [erroPassword, setErroPassword] = useState('');
   const [erroEmail, setErroEmail] = useState('');
+
+  const [isAuthenticating, setIsAuthenticating] = useState(false)
+
+  async function googleSignIn() {
+    try {
+      const resp = await GoogleSignin.signIn()
+      console.log(resp)
+    } catch(e: any) {
+      console.log(e)
+      Alert.alert("Entrar", "Não foi possível conectar com sua conta google")
+    }
+  }
 
   function handleLogin() {
     if (email === '' && password === '') {
@@ -68,7 +92,9 @@ export default function Login() {
           <SeparatorLine />
         </SeparatorContainer>
         <SocialIcons>
-          <Image source={LogoGoogle} />
+          <TouchableOpacity onPress={googleSignIn}  >
+            <Image source={LogoGoogle} />
+          </TouchableOpacity>
           <Image source={LogoGitHub} />
         </SocialIcons>
         <SignUpLink>
