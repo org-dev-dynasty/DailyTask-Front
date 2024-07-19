241c1587bc91725ad4054ef4b209396938b2fbaa
IsaiasCBLuz <isaiascbluz@gmail.com>
2024-07-18 23:12:32 -0300

    feat: Add ComfirmEmailResponse type

--- @clean/shared/infra/repositories/user_repository_http.ts
+++ @clean/shared/infra/repositories/user_repository_http.ts
@@ -1,22 +1,28 @@
 import { AxiosInstance } from 'axios';
 import { IUserRepository } from '../../../modules/interfaces/user_repo_interface';
 import { User } from '../../domain/entities/user';
-import { CreateUserResponse, LoginResponse } from '../../domain/types/user_responses';
+import { ComfirmEmailResponse, CreateUserResponse, LoginResponse } from '../../domain/types/user_responses';
 import { router } from 'expo-router';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export class UserRepositoryHttp implements IUserRepository {
     constructor(private readonly httpUser: AxiosInstance) {}
 
-    async create(user: User): Promise<User> {
+    async create(user: User): Promise<CreateUserResponse> {
         try {
-            const response = await this.httpUser.post(`${process.env.API_URL}/create-user`, user);
+            const response = await this.httpUser.post(`${process.env.API_URL}/create-user`, {
+                name: user.name,
+                email: user.email,
+                password: user.password,
+                accepted_terms: user.accepted_terms,
+                accepted_notifications_email: user.accepted_notifications_email
+            });
             if (response?.status == 201) {
                 console.log(user)
                 router.replace('/login');
             }
             console.log("RESPOSTA DA REQ CREATE" + response.data);
-            return response.data as User;
+            return response.data as CreateUserResponse;
         } catch (error: any) {
             throw new Error(error);
         }
@@ -39,4 +45,17 @@ export class UserRepositoryHttp implements IUserRepository {
             throw new Error(error);
         }
      }
+
+     async comfirmEmail(email: string, verificationCode: string): Promise<ComfirmEmailResponse> {
+        try {
+            const response = await this.httpUser.post<ComfirmEmailResponse>(`${process.env.API_URL}/confirm-user-email`, { email, verificationCode });
+            if (response.data) {
+                router.replace('/home');
+            }
+            console.log("RESPOSTA DA REQ CONFIRM EMAIL" + response.data);
+            return response.data;
+        } catch (error: any) {
+            throw new Error(error);
+        }
+    }
 }
