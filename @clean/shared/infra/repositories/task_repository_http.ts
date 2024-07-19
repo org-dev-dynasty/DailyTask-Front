f8cdbeacb1f04691c7821917e624e88fba17c27d
IsaiasCBLuz <isaiascbluz@gmail.com>
2024-07-18 23:12:23 -0300

    feat: Update update method in TaskRepositoryHttp
    This commit updates the `update` method in the `TaskRepositoryHttp` class to include the `task_id` parameter. This change is necessary to support the new `UpdateTaskUseCase` and allows for updating a task by its ID.

--- @clean/shared/infra/repositories/task_repository_http.ts
+++ @clean/shared/infra/repositories/task_repository_http.ts
@@ -31,7 +31,7 @@ export class TaskRepositoryHttp implements ITaskRepository {
         return response.data as GetAllTasksResponse;
     }
 
-    async update(task: Task): Promise<Task> {
+    async update(task_id: string, task: Task): Promise<Task> {
         try {
             const response = await this.httpTask.put(`${process.env.API_URL}/update-task`, task);
             if (response?.status == 200) {
@@ -53,6 +53,24 @@ export class TaskRepositoryHttp implements ITaskRepository {
             }
             console.log("RESPOSTA DA REQ DELETE" + response.data);
             return true;
+        } catch (error: any) {
+            throw new Error(error);
+        }
+    }
+
+    async taskByDay(day: string): Promise<GetAllTasksResponse> {
+        const response = await this.httpTask.get(`${process.env.API_URL}/get-task-by-day?task-day=${day}`);
+        return response.data as GetAllTasksResponse;
+    }
+
+    async updateStatus(task_id: string, status: string): Promise<Task> {
+        try {
+            const response = await this.httpTask.put(`${process.env.API_URL}/update-task-status?task-id=${task_id}`, { 'task-status': status });
+            if (response?.status == 200) {
+                console.log(task_id)
+            }
+            console.log("RESPOSTA DA REQ UPDATE STATUS" + response.data);
+            return response.data as Task;
         } catch (error: any) {
             throw new Error(error);
         }
