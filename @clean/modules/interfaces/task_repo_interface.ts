1f86fdb7ed4619629034e09cc514a6c4cabd6a29
IsaiasCBLuz <isaiascbluz@gmail.com>
2024-07-18 23:11:32 -0300

    refactor: Update task repository interface
    The task repository interface has been updated to include new methods for updating the task status and retrieving tasks by day.

--- @clean/modules/interfaces/task_repo_interface.ts
+++ @clean/modules/interfaces/task_repo_interface.ts
@@ -5,6 +5,8 @@ export interface ITaskRepository {
     create(task: Task): Promise<Task>;
     get(task_id: string): Promise<Task | null>;
     getAll(): Promise<GetAllTasksResponse>;
-    update(task: Task): Promise<Task>;
-    delete(task_id: string): Promise<boolean>;
+    update(task_id: string, task: Task): Promise<Task>;
+    delete(task_id: string): Promise<Task>;
+    taskByDay(day: string): Promise<Task>;
+    updateStatus(task_id: string, status: string): Promise<Task>;
 }
