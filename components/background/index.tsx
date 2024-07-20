0660fed1a60f97d7fd5beed25262b11c6050c6c6
IsaiasCBLuz <isaiascbluz@gmail.com>
2024-07-20 14:14:11 -0300

    chore: Update Background component with themeMode state and AsyncStorage
    This commit updates the Background component by adding the themeMode state using the useState hook. It also imports AsyncStorage from "@react-native-async-storage/async-storage" and useFocusEffect from "@react-navigation/native". These imports are necessary for implementing functionality related to storing data asynchronously and managing focus effects in the application. The changes include using the useFocusEffect hook to retrieve the themeMode value from AsyncStorage and update the themeMode state accordingly. This modification improves the functionality and performance of the Background component.

--- components/background/index.tsx
+++ components/background/index.tsx
@@ -1,17 +1,55 @@
+import { BackProps } from '@/interfaces/Background';
+import theme from '@/themes/theme';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useFocusEffect } from '@react-navigation/native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { useCallback, useState } from 'react';
+import { View } from 'react-native';
 
-export function Background({ children } : any) {
+export function Background(props: BackProps) {
+  const [themeModeS, setThemeModeS] = useState('dark'); // dark or light theme const
+  useFocusEffect(
+    useCallback(() => {
+        AsyncStorage.getItem('themeMode').then((value) => {
+            if (value) {
+                setThemeModeS(value);
+                // if (props.themeMode !== value) {
+                //   setThemeModeS(props.themeMode || value);
+                // }
+                console.log('themeMode', value);
+                console.log('props.themeMode', props.themeMode);
+            }
+        });
+    }
+, []));
+
   return (
-    <LinearGradient
-      colors={['#3C0B50', '#2E083D', '#0F0413']}
-      locations={[0, 0.28, 1]}
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      {children}
-    </LinearGradient>
+    <>
+      {themeModeS === 'light' ? (
+        <View 
+          style={{
+            backgroundColor: theme.COLORS.WHITE, 
+            flex:1, 
+            alignItems: 'center', 
+            justifyContent: 'center'
+          }}
+        >
+          {props.children}
+        </View>
+        ) : (
+          <LinearGradient
+            colors={['#3C0B50', '#2E083D', '#0F0413']}
+            locations={[0, 0.28, 1]}
+            style={{
+              flex: 1,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            {props.children}
+          </LinearGradient>
+        )}
+    </>
+    
   );
 }
