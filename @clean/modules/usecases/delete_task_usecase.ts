e60ac813ed703385ca261289f417965d20d79337
jotaapee <galhardojoaopedro@gmail.com>
2024-07-24 17:11:44 -0300

    chore: Update delete task use case return type to boolean
    This commit updates the return type of the `execute` method in the `DeleteTaskUseCase` class from `Task` to `boolean`. The change ensures consistency with the implementation and improves code clarity.

--- @clean/modules/usecases/delete_task_usecase.ts
+++ @clean/modules/usecases/delete_task_usecase.ts
@@ -4,7 +4,7 @@ import { ITaskRepository } from "../interfaces/task_repo_interface";
 export class DeleteTaskUseCase {
     constructor(private readonly task_repo: ITaskRepository) {}
 
-    async execute(task_id: string): Promise<Task> {
+    async execute(task_id: string): Promise<boolean> {
         return await this.task_repo.delete(task_id);
     }
 }
