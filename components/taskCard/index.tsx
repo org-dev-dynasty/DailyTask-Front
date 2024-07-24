36b15c00124147c0926553eba849b14b4487cbd5
jotaapee <galhardojoaopedro@gmail.com>
2024-07-24 18:13:36 -0300

    feat: Update TaskCard component to toggle task status
    The TaskCard component was modified to include a toggle functionality for the task status. This allows users to easily switch between active and inactive states for their tasks.

--- components/taskCard/index.tsx
+++ components/taskCard/index.tsx
@@ -1,13 +1,26 @@
 import { TaskCardProps } from "@/interfaces/TaskCard";
 import theme from "@/themes/theme";
-import { View, Text, ScrollView, Pressable, Animated, Dimensions } from "react-native";
+import { View, Text, ScrollView, Pressable, Animated, Dimensions, TouchableOpacity } from "react-native";
 import { CheckFat, Pencil } from "phosphor-react-native";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useContext } from "react";
+import { TaskContext } from "@/context/task_context";
 
 export const TaskCard = (props: TaskCardProps) => {
     const { width, height } = Dimensions.get('window');
     const [selected, setSelected] = useState(false);
     const slideAnim = useRef(new Animated.Value(-50)).current; // Valor inicial fora da tela
+    const { update } = useContext(TaskContext);
+    const [ statusActive, setStatusActive ] = useState(false);
+
+    async function handleActive() {
+        const newStatus = statusActive ? 'inactive' : 'active';
+        const response = await update(props.task_id, { task_status: newStatus });
+        console.log(response);
+        if (response) {
+            setStatusActive(!statusActive);
+            alert('Status atualizado com sucesso!');
+        }
+    }
 
     useEffect(() => {
         Animated.timing(slideAnim, {
@@ -18,35 +31,37 @@ export const TaskCard = (props: TaskCardProps) => {
     }, [selected]);
 
     return (
-        <View style={{flexDirection: "row", justifyContent: 'center'}}>
-            <View style={{width: '8%', height: height/100*8, backgroundColor: props.color, borderTopLeftRadius: 12, borderBottomLeftRadius: 12, zIndex: 1}}></View>
-            <Pressable 
-                onPress={() => setSelected(!selected)} 
-                style={{width: '85%', height: height/100*8, backgroundColor: props.color2, borderTopRightRadius: 12, borderBottomRightRadius: 12, paddingStart: 4, paddingTop: 4, zIndex: 1}}
+        <View style={{ flexDirection: "row", justifyContent: 'center' }}>
+            <View style={{ width: '8%', height: height / 100 * 8, backgroundColor: props.color, borderTopLeftRadius: 12, borderBottomLeftRadius: 12, zIndex: 1 }}></View>
+            <Pressable
+                onPress={() => setSelected(!selected)}
+                style={{ width: '85%', height: height / 100 * 8, backgroundColor: props.color2, borderTopRightRadius: 12, borderBottomRightRadius: 12, paddingStart: 4, paddingTop: 4, zIndex: 1 }}
             >
                 <Text>{props.title}</Text>
-                <ScrollView style={{maxHeight: height/100*6}}>
+                <ScrollView style={{ maxHeight: height / 100 * 6 }}>
                     <Text>{props.description}</Text>
                 </ScrollView>
             </Pressable>
             <Animated.View style={{
-                width: '15%', 
-                height: height/100*8, 
-                backgroundColor: theme.COLORS.WHITE, 
-                borderTopRightRadius: 12, 
-                borderBottomRightRadius: 12, 
-                position: 'absolute', 
-                right: -15, 
-                flexDirection: "column", 
-                alignItems: "flex-end", 
-                padding: 3, 
+                width: '15%',
+                height: height / 100 * 8,
+                backgroundColor: theme.COLORS.WHITE,
+                borderTopRightRadius: 12,
+                borderBottomRightRadius: 12,
+                position: 'absolute',
+                right: -15,
+                flexDirection: "column",
+                alignItems: "flex-end",
+                padding: 3,
                 gap: 4,
                 transform: [{
                     translateX: slideAnim
                 }],
             }}>
-                <CheckFat size={24} color={theme.COLORS.GREEN_200} style={{marginTop: 1}} />
-                <View style={{width: '100%', height: 1, backgroundColor: theme.COLORS.BLACK}}></View>
+                <TouchableOpacity onPress={handleActive}>
+                    <CheckFat size={24} color={theme.COLORS.GREEN_200} style={{ marginTop: 1 }} />
+                </TouchableOpacity>
+                <View style={{ width: '100%', height: 1, backgroundColor: theme.COLORS.BLACK }}></View>
                 <Pencil size={24} color={theme.COLORS.MAIN} style={{}} />
             </Animated.View>
         </View>
